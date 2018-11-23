24ec627452324a63b0ccb8488cb15685aef0630f
Snipin74 <josh.cove74@gmail.com>
2018-11-22 22:00:05 -0500

    f

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float Hspeed;
+    public float Vspeed;
+    public float aplitiude;
+
+
+    private Vector3 movePosition;
+    public float minY = 0.0f, maxY = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        //set default to moveposition
+        movePosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        //controls/ set to moveposition
+        movePosition.x += Hspeed;
+        movePosition.y = Mathf.Sin(Time.realtimeSinceStartup + Vspeed) * aplitiude + maxY - minY;
+        transform.position = movePosition;
+    }
+
+}
---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour {
+
+
+    public float timer;
+    public int newTarget;
+    public float speed;
+
+    public NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
