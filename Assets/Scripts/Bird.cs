7a1b994773dd0e12e1c07d7bf38bdbd8b3ae9de1
Snipin74 <josh.cove74@gmail.com>
2018-11-22 22:27:33 -0500

    bird are almost done

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -6,11 +6,13 @@ public class Bird : MonoBehaviour {
 
     public float Hspeed;
     public float Vspeed;
+    public float Zspeed;
     public float aplitiude;
 
 
     private Vector3 movePosition;
     public float minY = 0.0f, maxY = 10.0f;
+    public float minZ = 0.0f, maxZ = 20.0f;
 
     // Use this for initialization
     void Start()
@@ -23,6 +25,7 @@ public class Bird : MonoBehaviour {
     {
         //controls/ set to moveposition
         movePosition.x += Hspeed;
+        movePosition.z = Mathf.Sin(Time.realtimeSinceStartup + Zspeed) * aplitiude + maxZ - minZ;
         movePosition.y = Mathf.Sin(Time.realtimeSinceStartup + Vspeed) * aplitiude + maxY - minY;
         transform.position = movePosition;
     }
--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -2,18 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Fish : MonoBehaviour {
 
 
-    public float timer;
-    public int newTarget;
-    public float speed;
-
-    public NavMeshAgent nav;
-
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
