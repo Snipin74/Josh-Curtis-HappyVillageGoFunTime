cf3788f600c1dfa6edaf561392fbefff95d6f9bb
Snipin74 <josh.cove74@gmail.com>
2018-11-26 23:32:02 -0500

    fishing started
    spear is attached and rotating

---
+++ Assets/Scripts/FishingSpear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingSpear : MonoBehaviour {
+
+    
+    // Use this for initialization
+    void Start () {
+       
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            transform.Rotate(105, 0, 0);
+            
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            transform.Rotate(-105, 0, 0);
+        }
+    }
+
+}
