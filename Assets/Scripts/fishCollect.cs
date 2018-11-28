30558faebbd5ee9ce7f9f17ff4caa9a1967b255d
Snipin74 <josh.cove74@gmail.com>
2018-11-28 16:59:37 -0500

    FISHING DONE KINDA

--- Assets/Scripts/fishCollect.cs
+++ Assets/Scripts/fishCollect.cs
@@ -6,16 +6,29 @@ public class fishCollect : MonoBehaviour {
 
 
     public GameObject deadFish;
-    
+
+    public float DelayTime;
+    private float MaxTime = 3f;
+
+
+
+    private void Start()
+    {
+     
+    }
     private void OnTriggerStay(Collider other)
     {
         
-        if (other.gameObject.CompareTag("water"))
+            if (other.gameObject.CompareTag("water"))
+            {
+                Debug.Log("Fish");
+                deadFish.SetActive(true);
+            }
+            if (other.gameObject.CompareTag("Barrel"))
         {
-            Debug.Log("Fish");
-            deadFish.SetActive(true);
-
+            deadFish.SetActive(false);
         }
+    
     }
 
 }
