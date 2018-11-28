791e91e3b91985a9e949aa3aa7e6bdc4ed098821
Snipin74 <josh.cove74@gmail.com>
2018-11-27 19:44:31 -0500

    edits

--- Assets/Scripts/FishingSpear.cs
+++ Assets/Scripts/FishingSpear.cs
@@ -5,22 +5,61 @@ using UnityEngine;
 public class FishingSpear : MonoBehaviour {
 
     
+
+    public GameObject Spear;
+    public Vector3 currentPos;
+    public Vector3 endPos;
+    public Vector3 goBack;
+
+    public float distance = 30f;
+
+    private float LerpTime = 5;
+    private float currLerpTime = 0;
+
+    private bool keyHit = false;
+
     // Use this for initialization
     void Start () {
-       
+        currentPos = Spear.transform.position;
+        endPos = Spear.transform.position + Vector3.forward * distance;
+        goBack = Spear.transform.position + Vector3.forward / distance;
+
 	}
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetKeyDown(KeyCode.F))
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            transform.Rotate(105, 0, 0);
-            
+            transform.Rotate(110, 0, 0);
         }
-        if (Input.GetKeyUp(KeyCode.F))
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            transform.Rotate(-105, 0, 0);
+            transform.Rotate(-110, 0, 0);
         }
+
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    keyHit = true;
+        //}
+
+        //if (keyHit == true)
+        //{
+        //    currLerpTime += Time.deltaTime;
+        //    if(currLerpTime >= LerpTime)
+        //    {
+        //        currLerpTime = LerpTime;
+        //    }
+        //    float Perc = currLerpTime/LerpTime;
+        //    Spear.transform.position = Vector3.Lerp(currentPos, endPos, Perc);
+        //}
+
+        //if (Input.GetKeyUp(KeyCode.F))
+        //{
+        //    keyHit = false;
+        //} 
+ 
     }
 
 }
